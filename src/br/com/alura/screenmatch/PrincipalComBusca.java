4a1666a5490ba921365928b44d4ec5f4f827f130
Larifcoelho <larissafcoelho9@gmail.com>
2026-01-28 12:18:56 -0300

    updates

--- src/br/com/alura/screenmatch/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/PrincipalComBusca.java
@@ -1,17 +1,24 @@
 package br.com.alura.screenmatch;
-
+import java.util.Scanner;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+
+
 public class PrincipalComBusca {
     public static void main (String [] args) throws IOException, InterruptedException {
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite um filme: ");
+        var busca = scan.nextLine();
+
+        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=4c50b3e";
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?i=tt3896198&apikey=4c50b3e"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
